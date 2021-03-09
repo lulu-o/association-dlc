fa52e82c50823d1ba8b7317b699e1877e54ed0ee
Antoine GALPY <antoine@galpy.net>
2021-03-09 17:11:20 +0100

    Seed OK

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -2,22 +2,22 @@ class PartnersController < ApplicationController
   before_action :set_partner, only: %i[ show edit update destroy ]
 
   def index
-    @place = params[:place]
-    @radius = params[:radius].to_i
-    @partners = Partner.near(@place, @radius)
-    # Si aucun magasin n'est trouvé, on centre la carte sur l'endroit cherché avec un marker non cliquable
-    if @partners.first.nil?
-      @place_data = Geocoder.search(@place)[0].data
-      @markers = [{lat: @place_data["lat"], lng: @place_data["lon"], found: "none"}]
-    else
-      @markers = @partners.geocoded.map do |partner|
-        {
-          lat: partner.latitude,
-          lng: partner.longitude,
-          infoWindow: render_to_string(partial: "partners/info_window", locals: { partner: partner })
-        }
-      end
-    end
+    # @place = params[:place]
+    # @radius = params[:radius].to_i
+    # @partners = Partner.near(@place, @radius)
+    # # Si aucun magasin n'est trouvé, on centre la carte sur l'endroit cherché avec un marker non cliquable
+    # if @partners.first.nil?
+    #   @place_data = Geocoder.search(@place)[0].data
+    #   @markers = [{lat: @place_data["lat"], lng: @place_data["lon"], found: "none"}]
+    # else
+    #   @markers = @partners.geocoded.map do |partner|
+    #     {
+    #       lat: partner.latitude,
+    #       lng: partner.longitude,
+    #       infoWindow: render_to_string(partial: "partners/info_window", locals: { partner: partner })
+    #     }
+    #   end
+    # end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 require "open-uri"
 
 puts "Cleaning Database"
-puts "Cleaning Associations"
-Association.destroy_all
 puts "Cleaning Users"
 User.destroy_all
+puts "Cleaning Associations"
+Association.destroy_all
 puts "Database Cleaned"
 
 association_1 = Association.create({name: "Association DLC", address: "14 rue Malherbe", zipcode: "44000", city: "NANTES", latitude: "47.2178075", longitude: "-1.5477781"})
 
 puts "Adding Users"
-user_1 = User.create({association: association_1, first_name: "Erika", last_name: "NAUDON", address: "25 rue du château", zipcode: "44680", city: "Saint Mars de Coutais", email: "erika@dlc.org", password: "azerty", active: true, admin: true})
-user_2 = User.create({association: association_1, first_name: "Lucrece", last_name: "GUGELOT", address: "Place de l'église", zipcode: "44680", city: "Saint Mars de Coutais", email: "lucrece@dlc.org", password: "azerty", active: true, admin: true})
-user_3 = User.create({association: association_1, first_name: "Charlotte", last_name: "NOVELLO", address: "53 la Guinanderie", zipcode: "44680", city: "Saint Mars de Coutais", email: "charlotte@dlc.org", password: "azerty", active: true, admin: true})
-user_4 = User.create({association: association_1, first_name: "Céline", last_name: "MEILLERAY", address: "10 rue Félix Faure", zipcode: "44000", city: "Nantes", email: "celine@dlc.org", password: "azerty", active: true, admin: true})
-user_5 = User.create({association: association_1, first_name: "Sonia", last_name: "FRICAUD", address: "10 rue Félix Faure", zipcode: "44000", city: "Nantes", email: "celine@dlc.org", password: "azerty", active: true, admin: true})
+user_1 = User.create({my_association: association_1, first_name: "Erika", last_name: "NAUDON", address: "25 rue du château", zipcode: "44680", city: "Saint Mars de Coutais", email: "erika@dlc.org", password: "azerty", active: true, admin: true})
+user_2 = User.create({my_association: association_1, first_name: "Lucrece", last_name: "GUGELOT", address: "Place de l'église", zipcode: "44680", city: "Saint Mars de Coutais", email: "lucrece@dlc.org", password: "azerty", active: true, admin: true})
+user_3 = User.create({my_association: association_1, first_name: "Charlotte", last_name: "NOVELLO", address: "53 la Guinanderie", zipcode: "44680", city: "Saint Mars de Coutais", email: "charlotte@dlc.org", password: "azerty", active: true, admin: true})
+user_4 = User.create({my_association: association_1, first_name: "Céline", last_name: "MEILLERAY", address: "10 rue Félix Faure", zipcode: "44000", city: "Nantes", email: "celine@dlc.org", password: "azerty", active: true, admin: true})
+user_5 = User.create({my_association: association_1, first_name: "Sonia", last_name: "FRICAUD", address: "10 rue du Père Noël", zipcode: "44000", city: "Nantes", email: "sonia@dlc.org", password: "azerty", active: true, admin: true})
 puts "End of Adding Users"
