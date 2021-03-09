52df530d15a28a21e611bd804671a5068a3e009d
Antoine GALPY <antoine@galpy.net>
2021-03-09 15:25:10 +0100

    Push Gemfile avec geocoder et seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning Brands"
+Brand.destroy_all
+puts "Cleaning Associations"
+Association.destroy_all
+
+
+Brand.create!([
+  {name: "Carrefour", logo: "https://e7.pngegg.com/pngimages/592/86/png-clipart-carrefour-online-marketing-business-hypermarket-marketing-text-retail.png"},
+  {name: "Systeme U", logo: "https://img2.freepng.fr/20180713/xfo/kisspng-systme-u-supermarket-hypermarket-super-u-et-driv-logo-bea-cukai-5b488a12a2a167.7599448815314805946662.jpg"}
+])
+Association.create!([
+  {name: "Association DLC", address: "16 Rue Malherbe", zipcode: "44000", city: "NANTES", latitude: 47.2178075, longitude: -1.5477781}
+])
