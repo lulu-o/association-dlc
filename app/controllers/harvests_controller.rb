c00be605b54d950e9ef2bb2b93662b148061cb0f
marion petitprez <marion.petitprez@gmail.com>
2021-03-10 18:23:23 +0100

    harvest index en cours

--- app/controllers/harvests_controller.rb
+++ app/controllers/harvests_controller.rb
@@ -1,6 +1,14 @@
 class HarvestsController < ApplicationController
   def index
     @harvests = Harvest.joins(:partner).where('date >= ?', DateTime.now)
+
+    # Retrieve harvests needing people - to be joined with upper query
+    # Facon degueulasse
+    # @emergency_harvest = Harvest.all.select do |harvest|
+    #   harvest.harvesters_number > harvest.harvesters.count
+    # end
+    # Facon plus propre ne fonctionne pas - a creuser
+    # Harvest.where('harvesters_number > harvesters.count')
   end
 
   def number_needed
