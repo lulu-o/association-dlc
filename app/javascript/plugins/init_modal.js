dd4f0f2c19a79cef02c0439f7ba972d8df218317
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-03-12 00:20:36 +0100

    retouches

--- app/javascript/plugins/init_modal.js
+++ app/javascript/plugins/init_modal.js
@@ -1,31 +1,32 @@
 const initModal = () => {
   const modalWindow = document.getElementById("modal-window");
   if (modalWindow) {
-    const buttons = document.querySelectorAll(".date-action > div");
-    // Eléments de la modale (nom du magasin, date, bouton annuler)
-    let modalShop = document.getElementById("modal-shop");
-    let modalSchedule = document.getElementById("modal-schedule");
+    const shop = document.getElementById("partner-name").innerText;
+    // Eléments de la modale (message, nom du magasin, date, boutons)
+    const introTag = document.getElementById("intro-text");
+    const modalShop = document.getElementById("modal-shop");
+    const modalSchedule = document.getElementById("modal-schedule");
     const cancelButton = document.getElementById("modal-cancel");
+    const confirmButton = document.getElementById("modal-confirm");
+    //Tous les boutons d'action des dates
+    const buttons = document.querySelectorAll(".date-action > div");
 
     buttons.forEach((button) => {
       button.addEventListener('click', () => {
         button.classList.add('pop');
         modalWindow.classList.add("z10");
-        const shop = document.getElementById("partner-name").innerText;
         const date = button.parentElement.parentElement.querySelector('.date').innerText;
-        const introTag = document.getElementById("intro-text");
-        // On vérifie la classe css du bouton pour afficher le bon message
+        // Vérifier la classe css du bouton pour afficher le bon message
         if (button.classList.contains('cancel')) {
-          let introText = "Souhaitez-vous vous désinscrire pour la récolte :";
-          introTag.innerText = introText;
+            introTag.innerText = "Souhaitez-vous vous désinscrire pour la récolte :";
         } else {
-          let introText = "Confirmez votre inscription pour la récolte :";
-          introTag.innerText = introText;
+            introTag.innerText = "Confirmez votre inscription pour la récolte :";
         }
 
-        // On insère les données magasin + date
+        // Insérer les données magasin + date
         modalShop.innerText = shop;
         modalSchedule.innerText = date;
+        // Fermer la modale et rétablir l'état du bouton de la date
         setTimeout(() => {
           button.classList.remove('pop');
           modalWindow.classList.remove("transparent");
@@ -33,16 +34,22 @@ const initModal = () => {
       });
     });
 
-    // Au clic sur Annuler, on masque la modale
+    // Au clic sur Annuler, masquer la modale
     cancelButton.addEventListener('click', () => {
       modalWindow.classList.add("transparent");
       setTimeout(() => {
         modalWindow.classList.remove("z10");
-      }, 1000);
+      }, 700);
+    });
+
+    // Au clic sur Confirmer, inscrire le user (à terminer)
+    confirmButton.addEventListener('click', () => {
+      modalWindow.classList.add("transparent");
+      setTimeout(() => {
+        modalWindow.classList.remove("z10");
+      }, 700);
     });
   }
-
-
 
 };
 
