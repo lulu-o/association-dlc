97c044e132a48c9213d896808b756fdbea10dfbf
Antoine GALPY <antoine@galpy.net>
2021-03-11 15:58:02 +0100

    ListHarvestsGroupByDate

--- app/controllers/harvests_controller.rb
+++ app/controllers/harvests_controller.rb
@@ -1,6 +1,21 @@
 class HarvestsController < ApplicationController
   def index
     @harvests = Harvest.joins(:partner).where('date >= ?', DateTime.now).order(:date)
+    urgent_harvests_query = <<~SQL
+      WITH harvesters_count AS
+      (SELECT harvest_id AS id, count(id) AS compteur FROM harvesters GROUP BY harvesters.harvest_id ORDER BY compteur DESC)
+      SELECT
+          harvests.id,
+          harvests.date,
+          harvests.hour,
+          harvests.partner_id,
+          harvests.harvesters_number
+      FROM harvests
+      LEFT OUTER JOIN harvesters_count ON harvesters_count.id = harvests.id
+      WHERE (harvesters_count.compteur < harvests.harvesters_number OR harvesters_count.id IS NULL)
+      SQL
+
+      @urgents_harvests = Harvest.find_by_sql Arel.sql(urgent_harvests_query)
 
     # Retrieve harvests needing people - to be joined with upper query
     # Facon degueulasse
@@ -11,3 +26,8 @@ class HarvestsController < ApplicationController
     # Harvest.where('harvesters_number > harvesters.count')
   end
 end
+
+
+
+
+
