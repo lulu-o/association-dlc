856d07135bb0a6390cf3a60a1c04316c77153747
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-03-14 16:26:28 +0100

    optimisation recherche

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+Fonctionnement Recherche
+
+Affichage du formulaire :
+Par défaut, le formulaire affiche l'adresse du user et un rayon de 5km
+Les deux champs sont obligatoires et le submit ne se produira que s'ils sont renseignés
+
+Géoloc :
+Au clic sur le marker position, le champ adresse affiche "ma position" tandis que les coordonnées sont stockées dans l'attribut data-coords du champ
+L'attribut data-search-type, qui est "text" par défaut est modifié pour préciser le contexte géoloc
+Au moment du submit, on récupère l'attribut data-coords pour le passer en paramètre
+
+Résultats - Cas identifiés
+Au premier affichage, aucun param n'est passé. On affiche donc arbitrairement l'adresse du user avec un rayon de 10 km.
--- app/javascript/plugins/init_search.js
+++ app/javascript/plugins/init_search.js
@@ -1,9 +1,32 @@
 const initSearch = () => {
-  const searchButton = document.getElementById("search");
-  if (searchButton) {
-    searchButton.addEventListener('click', () => {
-      const place = document.getElementById("place").innerText;
-      const result = Geocoder.search(place);
+  const searchForm = document.getElementById("geo-search");
+
+  // GEOLOC : Afficher "ma position" dans le formulaire et remplacer par les coordonnées au moment du submit
+  if (searchForm) {
+    const place = document.getElementById("place");
+    const searchType = document.getElementById("search-type");
+    const locationLink = document.getElementById("geoloc");
+    // const submit = document.getElementById("search-submit");
+
+    locationLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      navigator.geolocation.getCurrentPosition((data) => {
+        const currentPosition = [data.coords.latitude, data.coords.longitude];
+        place.value = "Ma position";
+        place.setAttribute("data-coords", currentPosition);
+        searchType.value = "geolocation";
+      });
+    });
+
+    searchForm.addEventListener('submit', (event) => {
+      if (searchType.value === "geolocation") {
+        const locationData = place.getAttribute("data-coords");
+        place.value = locationData;
+      }
+    });
+
+    place.addEventListener('keyup', () => {
+      searchType.value = "text";
     });
   }
 };
