6b8b7721a66bac6be2a2aaa19e8d9371393fc2e4
marion petitprez <marion.petitprez@gmail.com>
2021-03-15 23:04:45 +0100

    implementation flatpick

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -1,4 +1,6 @@
 class Distribution < ApplicationRecord
   belongs_to :harvest
   belongs_to :user
+
+  validates :date, :hour, :address, :zipcode, :city, :baskets_number, :description, :need_bag, :need_cooler, :informations, presence: true
 end
