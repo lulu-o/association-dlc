98c185c8a07a6bb2fad71413aaf94599bb96e5c8
Antoine GALPY <antoine@galpy.net>
2021-03-15 14:20:35 +0100

    QueryToPurposeHarvestToDistribute

--- app/controllers/distributions_controller.rb
+++ app/controllers/distributions_controller.rb
@@ -1,6 +1,14 @@
 class DistributionsController < ApplicationController
 	def new
     @distribution = Distribution.new # needed to instantiate the form_for
+    my_harvests_to_distribute = <<~SQL
+      SELECT harvests.id, harvests.date, partners.name FROM harvests
+      INNER JOIN harvesters ON harvesters.harvest_id = harvests.id
+      INNER JOIN partners ON partners.id = harvests.partner_id
+      WHERE harvesters.user_id = ? AND (harvests.date > ? OR harvests.date = ?)
+      ORDER BY harvests.date
+    SQL
+    @harvests = Harvest.find_by_sql([my_harvests_to_distribute, current_user.id, Date.today - 2.days, Date.today])
   end
 
   def create
