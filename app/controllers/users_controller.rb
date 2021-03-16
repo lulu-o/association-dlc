976500cd8e0bf24efdbbb8f21b761674d95d34b2
marion petitprez <marion.petitprez@gmail.com>
2021-03-16 16:05:32 +0100

    date planning ordre chronologique

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @harvesters = Harvester.where("user_id = ?", current_user.id)
+    # @harvesters = Harvester.where("user_id = ?", current_user.id)
+    # @harvesters = Harvest.includes(:harvesters).where( harvesters: {user: current_user}).order(:date) 
+    @harvesters = Harvester.includes(:harvest).where("user_id = ? AND harvests.date >= ?", current_user.id, DateTime.current.to_date).order('harvests.date') 
+
+    
     @distributions = Distribution.where("user_id = ?", current_user.id).order(:date, :hour).where('date >= ?', DateTime.current.to_date)
   end
 end
