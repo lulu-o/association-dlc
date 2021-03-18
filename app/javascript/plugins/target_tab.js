7c1083d929f93c23c7781ad195c961ee40824ada
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-03-18 22:33:48 +0100

    cancel from profile

--- app/javascript/plugins/target_tab.js
+++ app/javascript/plugins/target_tab.js
@@ -9,7 +9,18 @@ const targetTabs = () => {
     target.setAttribute('aria-expanded', 'true');
     const block = document.getElementById(blockTarget);
     block.classList.add('show');
+
+    const cancelFromProfile = document.querySelectorAll('div.cancel');
+    cancelFromProfile.forEach((cancelButton) => {
+      cancelButton.addEventListener('click', () => {
+        const splitted = document.location.href.split('?');
+        const newLoc = splitted[0] + "?target=planning";
+        history.pushState('', '', newLoc);
+      });
+    });
   }
+
+
 };
 
 export { targetTabs };
