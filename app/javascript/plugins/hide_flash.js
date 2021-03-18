6b7d4897b126444b1d798add20c2587ffd35ed86
Guillaume Ferchaud <guillaumeferchaud@hotmail.fr>
2021-03-18 15:48:17 +0100

    boutons radius

--- app/javascript/plugins/hide_flash.js
+++ app/javascript/plugins/hide_flash.js
@@ -3,7 +3,6 @@ const hideFlash = () => {
   const flashes = document.querySelectorAll(".alert");
   flashes.forEach((flash) => {
     setTimeout(function () {
-      body.classList.remove("preload");
       flash.style.opacity = 0;
     }, 1000);
   })
